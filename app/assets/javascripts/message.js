f03ea0b206e53aa88b3ee9310879953f58bf7cd2
Tsujimoto696 <chocolate.rich13@gmail.com>
2019-08-22 21:50:39 +0900

    非同期通信実装後修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,126 +1,48 @@
 $(document).on("turbolinks:load", function() {
 
   function buildHTML(message) {
-
     // 画像がアップされないときは<img src = "null">となり余計なサムネが表示されることを防ぐ
-    if (message.image) {
-      var imageEle = `<img src="${message.image}">`;
-    } else {
-      var imageEle = '';
-    }
+    var image = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
 
     var html =
-      `<div class = "message">
-        <div class = "upper-message__data-message-id>${message.id}></div>
+      `<div class = "message" data-id = "${message.id}">
         <div class = "upper-message__user-name">${message.user_name}</div>
         <div class = "upper-message__date">${message.date}</div>
         <div class = "lower-message__content">${message.content}</div>
-        <div class = "lower-message__image">${imageEle}</div>
+        <div class = "lower-message__image">${image}</div>
       </div>`;
     return html;
   }
 
-  // 最新のメッセージが表示されるように自動でスクロールする
-  function autoScrollToBottom(){
-    var targetY = $('.chat-main__body--messages-list').height();
-    $('.chat-main__body').scrollTop(targetY);
-  }
-
-  // 自動更新のためのAjaxをメソッド化
-  function getLatestMessages(){
-    // 特定のグループの最後に投稿されたメッセージのidを取得する
-    // まだメッセージが投稿されていない場合は、0を代入する
-    // ||の左側の要素があればそれを代入、なければ右側の値を代入
-    var lastMessageID = $('.chat-main__body--message').last().data('message-id') || 0;
-    // APIに最後のメッセージのidを送り、そのidより大きいメッセージがあれば返してもらう
-    $.ajax({
-      type: 'GET',
-      url: './messages',
-      data: {
-        lastMessageID: lastMessageID
-      },
-      dataType: 'json'
-    })
-
-    .done(function(data) {
-      // 配列dataの要素数が1以上のときのみHTMLを組成する
-      if (data.length){
-        data.forEach(function(message_add){
-          var html = buildHTML(message_add);
-          $('.chat-main__body--messages-list').append(html);
-        });
-        autoScrollToBottom();
-      }
-    })
-
-    .fail(function() {
-      alert('エラーが生じました');
-    });
-    // Turbolinksを止めないためにfalseを返しておく
-    return false;
-  }
-
-
-
-  // ファイル選択時にフォームを自動で送信する
-  $('#message_image').on('change', function(){
-    $(this).parents('#new_message').submit();
-  });
-
   // フォームの非同期通信
-  $('#new_message').on('submit', function(e) {
-    // javascriptで作成したフラッシュメッセージを削除
-    $('.notice-succsess').remove();
-    $('.notice-error').remove();
+  $('#new_message').on('submit', function (e) {
     e.preventDefault();
-    var textField = $('#message_body'),
-        fileField = $('#message_image'),
-        formData = new FormData(this);
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
     $.ajax({
-      type: 'POST',
-      url: $(this).attr('action'),
-      data: formData,
-      // Ajaxがdataを整形しない指定
-      processData: false,
-      contentType: false,
-      dataType: 'json'
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
     })
-    //成功時
+    //送信成功時
     .done(function(data) {
       var html = buildHTML(data);
       $('.messages').append(html);
-      // メッセージを投稿するたびに自動で最下層にスクロール
-      $('#scroll').animate({scrollTop: $('#scroll')[0].scrollHeight}, 'fast');
-      //$('#scroll').animate({ scrollTop: $('#scroll')[0].scrollHeight});
-      //autoScrollToBottom();
+      //$('.messages').removeAttr('disabled');
       // javascriptでフラッシュメッセージを作成
-      var notice = $('<p class = "notice-succsess">').append('新規メッセージが送信されました');
-      $('.notice').append(notice);
-      textField.val('');
-      fileField.val('');
+      //var notice = $('<p class = "notice-succsess">').append('新規メッセージが送信されました');
+      //$('.notice').append(notice);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      return false;
     })
-    
-    .fail(function() {
-      var alert = $('<p class = "notice-error">').append('メッセージを入力して下さい');
-      $('.notice').append(alert);
-    });
+    // 送信失敗時
+    .fail(function(){
+	    alert('error');
+	  });
     // Turbolinksを止めないためにfalseを返しておく
     return false;
   });
-
-  // 10秒間隔で、インターバル中に投稿されたメッセージを非同期で取得し表示する
-  // 別ページに遷移した際にclearIntervalでsetIntervalを止める必要があるため、setIntervalを変数化する
-  var autoReload = setInterval(function(){
-
-    // URLがmessages#indexのパスと等しいときにのみ、Ajaxを起動する
-    if(location.pathname.match(/\/groups\/\d+\/messages/)){
-      getLatestMessages();
-
-    } else {
-    // 別ページに遷移したらsetIntervalを停止させる
-      clearInterval(autoReload);
-    }
-
-  }, 10000);
-
 });
